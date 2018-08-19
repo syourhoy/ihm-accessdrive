2a8c1425d9aa56119813cb3e15181aa7b96c2c28
Thierry Loris <thierr_l@etna-alternance.net>
2018-08-19 14:49:29 +0200

    Requete nuuméro téléphone

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { UsersServicesProvider } from '../../providers/users-services/users-services';
 @Component({
   selector: 'page-home',
@@ -7,15 +9,26 @@ import { UsersServicesProvider } from '../../providers/users-services/users-serv
 })
 
 export class HomePage {
-  
+
   form = {number:"",save:false}
 
 
-  constructor(public navCtrl: NavController, public service: UsersServicesProvider) {
+  constructor(public navCtrl: NavController, public http: Http, public service: UsersServicesProvider) {
 
   }
   logForm() {
-    console.log(this.form)
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let body = {
+      number : this.form.number
+    };
+
+    this.http.post('http://127.0.0.1', JSON.stringify(body), {headers: headers})
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log(data);
+    });
+    console.log(body);
   }
 
 }
